badf4cbf691e9797bf5530231d9ebe42a8a926ef
wendelldsouza17 <wendelldsouza023@gmail.com>
2026-08-29 19:08:50 +0530

    feat: add users database model

---
+++ backend/app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from backend.app.database.connection import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
